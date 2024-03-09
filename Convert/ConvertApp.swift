5951cbc084ed69e1f0b2501cf9aeae8eaf1d4a4c
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2024-03-09 01:01:38 -0700

    Initial Commit

---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Rahul Shah on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
