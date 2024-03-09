80a04d671c935cdb69f34576d70e97d130d8a643
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2024-03-09 01:45:25 -0700

    Completed

--- Convert/ContentView.swift
+++ Convert/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputString: String = ""
+    @State private var inputUnit: String = "Kilometers"
+    @State private var outputUnit: String = "Meters"
+    @FocusState private var inputField: Bool
+    
+    var result: Double {
+        guard let input = Double(inputString) else { return 0 }
+        if inputUnit == outputUnit {
+            return input
+        } else if inputUnit == "Kilometers" && outputUnit == "Meters" {
+            return input * 1000
+        } else {
+            return input / 1000
         }
-        .padding()
+    }
+    
+    let units: [String] = ["Kilometers", "Meters"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("from") {
+                    TextField("Enter your value", text: $inputString).keyboardType(.decimalPad)
+                        .focused($inputField)
+                    Picker("Select Unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("To") {
+                    Picker("Select Unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    Text(String(format: "%.3f", result) + " \(outputUnit)")
+                }
+                
+            }.navigationTitle("Convert")
+                .toolbar {
+                    if inputField {
+                        Button("Done") {
+                            inputField = false
+                        }
+                    }
+                }
+        }
     }
 }
 
